f23482670faef6e2f72db591a19a19075d7693ec
mcinstinct <mc@mail.instinct.com>
2026-09-16 19:42:28 -0600

    Default globe to maximum zoom-out

--- globe.js
+++ globe.js
@@ -67,7 +67,7 @@ function resize() {
   canvas.width = size * dpr; canvas.height = size * dpr
   canvas.style.width = `${size}px`; canvas.style.height = `${size}px`
   ctx = canvas.getContext('2d'); ctx.setTransform(dpr, 0, 0, dpr, 0, 0)
-  scale = size * 0.39
+  scale = size * 0.28
   projection = geoOrthographic().translate([size / 2, size / 2]).scale(scale).clipAngle(90).precision(.4).rotate(rotation)
   path = geoPath(projection, ctx)
 }
